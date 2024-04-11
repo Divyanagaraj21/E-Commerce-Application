6bce8508b9cfd0f1e0ead4c61b3b8fd28d23be99
Divyanagaraj21 <divyanagaraj2122@gmail.com>
2024-04-11 20:52:06 +0530

    project created-ui

---
+++ e-commerce-application-UI/src/Public/Login.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { CgProfile } from "react-icons/cg";
+import { IoIosArrowDown } from "react-icons/io";
+import { CiSearch } from "react-icons/ci";
+import { BsBox } from "react-icons/bs";
+import { GoHeart } from "react-icons/go";
+
+function Login() {
+  return (
+<div>
+
+    <form className='mt-32 mx-auto border-2 border-slate-4008 w-96 h-60 flex flex-col justify-evenly items-center rounded-md
+    scale-110'>
+      
+
+    <div className='text-yellow-800 font-bold text-lg text-center w-full bg-green-300 -mt-4 rounded-t'>Welcome Back!</div>
+    
+    <input className='border-2 border-slate-400 px-5 py-1' type="text" placeholder='Enter email'/>
+    <input className='border-2 border-slate-400 px-5 py-1' type="password" placeholder='Enter password'/>
+    <input className="border-2 border-yellow-400 px-5 py-1 cursor-pointer rounded-md font-medium transition ease—in-out
+    duration-300 hover:bg-yellow-200 hover: text-sky-708 hover: border-yellow-200 hover:-translate-y-1" type="submit" value='Log In'/>
+    <a className='text-base font-normal' href=''>forgot password?</a>
+    </form>
+    </div>
+  )
+}
+
+export default Login
