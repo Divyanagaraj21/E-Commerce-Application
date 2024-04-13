7d9734b231c471f45ad6f8c60d43f655bfa8c7b7
Divyanagaraj21 <divyanagaraj2122@gmail.com>
2024-04-13 12:47:19 +0530

    project created-API

---
+++ E-Commerce-Application/src/main/java/com/retail/requestdto/UserRequest.java
@@ -0,0 +1,51 @@
+package com.retail.requestdto;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public class UserRequest {
+	@NotNull
+	private String name;
+	@Email(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$",message="Invalid emailId")
+	private String email;
+	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Password must"
+			+ " contain at least one letter, one number, one special character")
+	private String password;
+	private boolean isEmailvarified;
+	private boolean isDeleted;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isEmailvarified() {
+		return isEmailvarified;
+	}
+	public void setEmailvarified(boolean isEmailvarified) {
+		this.isEmailvarified = isEmailvarified;
+	}
+	public boolean isDeleted() {
+		return isDeleted;
+	}
+	public void setDeleted(boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+
+}
---
+++ E-Commerce-Application/src/main/java/com/retail/responsedto/UserResponse.java
@@ -0,0 +1,11 @@
+package com.retail.responsedto;
+
+public class UserResponse {
+	private int userId;
+	private String name;
+	private String userName;
+	private String email;
+	private boolean isDeleted;
+	
+
+}
