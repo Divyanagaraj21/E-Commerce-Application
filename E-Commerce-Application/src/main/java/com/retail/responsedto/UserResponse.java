12fab71575ebad95966cad7d2a958bc34983dbc7
Divyanagaraj21 <divyanagaraj2122@gmail.com>
2024-04-13 12:53:12 +0530

    dev

---
+++ E-Commerce-Application/src/main/java/com/retail/responsedto/UserResponse.java
@@ -0,0 +1,42 @@
+package com.retail.responsedto;
+
+public class UserResponse {
+	private int userId;
+	private String name;
+	private String userName;
+	private String email;
+	private boolean isDeleted;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public boolean isDeleted() {
+		return isDeleted;
+	}
+	public void setDeleted(boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+	
+	
+
+}
